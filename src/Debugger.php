3e8e9a7791dac4ec60da33ee020ae15fa10be743
Nick Sagona <nick@nolainteractive.com>
2023-09-02 23:10:10 -0500

    Update dependencies

--- src/Debugger.php
+++ src/Debugger.php
@@ -25,7 +25,7 @@ use ReturnTypeWillChange;
  * @author     Nick Sagona, III <dev@nolainteractive.com>
  * @copyright  Copyright (c) 2009-2023 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
- * @version    1.3.0
+ * @version    1.3.2
  */
 class Debugger implements \ArrayAccess, \Countable, \IteratorAggregate
 {
