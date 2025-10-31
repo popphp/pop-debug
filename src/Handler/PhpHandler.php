4f85f05bce64ccb92857d180e8a5d8a0ff3c7b5f
Nick Sagona <nick@noladev.com>
2025-10-31 11:11:32 -0500

    Update version and copyright

--- src/Handler/PhpHandler.php
+++ src/Handler/PhpHandler.php
@@ -117,7 +117,6 @@ class PhpHandler extends AbstractHandler
         E_USER_DEPRECATED   => 'E_USER_DEPRECATED',
         E_DEPRECATED        => 'E_DEPRECATED',
         E_RECOVERABLE_ERROR => 'E_RECOVERABLE_ERROR',
-        E_STRICT            => 'E_STRICT',
         E_USER_NOTICE       => 'E_USER_NOTICE',
         E_USER_WARNING      => 'E_USER_WARNING',
         E_USER_ERROR        => 'E_USER_ERROR',
